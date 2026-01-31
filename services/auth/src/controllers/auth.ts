9df842cd98c77049ba109b3f4b76541bc78e0cb9
Madhukar <maddymustfocus@gmail.com>
2026-01-31 19:30:02 +0530

    feat(auth): finalize register w/ upload and add login controller

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,7 +1,10 @@
+import axios from "axios";
+import getBuffer from "../utils/buffer.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
 import { sql } from "../utils/db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 export const registerUser = TryCatch(async (req, res, next) => {
@@ -50,8 +53,60 @@ export const registerUser = TryCatch(async (req, res, next) => {
     
         registeredUser = user;
       }
-    
-
+      const token = jwt.sign(
+        { id: registeredUser?.user_id },
+        process.env.JWT_SEC as string,
+        {
+          expiresIn: "15d",
+        }
+      );
+      res.json({
+        message: "user Registered",
+        registeredUser,
+        token
+      });
+  });
   
-    res.json(email);
+  export const loginUser = TryCatch(async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      throw new ErrorHandler(400, "Please fill all details");
+    }
+  
+    const user = await sql`
+    SELECT u.user_id, u.name, u.email, u.password, u.phone_number, u.role, u.bio, u.resume, u.profile_pic, u.subscription, ARRAY_AGG(s.name) FILTER (WHERE s.name IS NOT NULL) as skills FROM users u LEFT JOIN user_skills us ON u.user_id = us.user_id
+    LEFT JOIN skills s ON us.skill_id = s.skill_id
+    WHERE u.email = ${email} GROUP BY u.user_id;
+    `;
+  
+    if (user.length === 0) {
+      throw new ErrorHandler(400, "Invalid credentials");
+    }
+  
+    const userObject = user[0];
+  
+    const matchPassword = await bcrypt.compare(password, userObject.password);
+  
+    if (!matchPassword) {
+      throw new ErrorHandler(400, "Invalid credentials");
+    }
+  
+    userObject.skills = userObject.skills || [];
+  
+    delete userObject.password;
+  
+    const token = jwt.sign(
+      { id: userObject?.user_id },
+      process.env.JWT_SEC as string,
+      {
+        expiresIn: "15d",
+      }
+    );
+  
+    res.json({
+      message: "user Loggedin",
+      userObject,
+      token,
+    });
   });
