acb3559232e064e4dbf6058b709c4ac80ec8e6e1
Madhukar <maddymustfocus@gmail.com>
2026-02-03 03:59:08 +0530

    feat(auth): implement forgot password flow with redis expiration

--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.js";
+import { forgotPassword, registerUser, loginUser } from "../controllers/auth.js";
 import uploadFile from "../middleware/multer.js";
 
 
 const router = express.Router();
 
 router.post("/register", uploadFile, registerUser);
-
+router.post("/login", loginUser);
+router.post("/forgot", forgotPassword);
 
 export default router;
