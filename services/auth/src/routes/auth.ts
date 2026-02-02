a1cecf57b768d647e6ac0a57331dcd6c6ce5ef7e
Madhukar <maddymustfocus@gmail.com>
2026-02-03 04:10:26 +0530

    feat(auth): implement reset password and finalize auth service

--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { forgotPassword, registerUser, loginUser } from "../controllers/auth.js";
+import { forgotPassword, registerUser, loginUser, resetPassword } from "../controllers/auth.js";
 import uploadFile from "../middleware/multer.js";
 
 
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/register", uploadFile, registerUser);
 router.post("/login", loginUser);
 router.post("/forgot", forgotPassword);
+router.post("/reset/:token", resetPassword);
 
 export default router;
