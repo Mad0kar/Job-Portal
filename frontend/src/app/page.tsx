0cdab51b5d6281d18c3222718a52627af53ddd63
Madhukar <maddymustfocus@gmail.com>
2026-03-07 18:04:05 +0530

    feat(frontend): initialize Next.js app with shadcn, theme provider, and navbar

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const Home = () => {
+  return (
+    <div>
+      <Button>Shadcn Button</Button>
+    </div>
+  )
+}
+
+export default Home
